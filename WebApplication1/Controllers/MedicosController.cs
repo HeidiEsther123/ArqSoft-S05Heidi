765a0017708623b1d6f91c7d0952eb58e0bef482
HeidiEsther123 <heidi.pena@tecdesoftware.edu.mx>
2026-06-17 10:53:20 -0600

    feat: Integrar proyecto Citas_App.Api correctamente

---
+++ WebApplication1/Controllers/MedicosController.cs
@@ -0,0 +1,27 @@
+﻿using Citas_App.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Citas_App.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MedicosController : ControllerBase
+    {
+        private readonly MedicoService _service;
+
+        public MedicosController(MedicoService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() => Ok(_service.ObtenerTodos());
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var medico = _service.ObtenerPorId(id);
+            return medico == null ? NotFound() : Ok(medico);
+        }
+    }
+}
